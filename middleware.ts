4a03b9ed4388be99ee75ff10edd87748ddeef47d
mahalluapp <mahalluapp@gmail.com>
2024-02-27 18:04:03 +0530

    correctMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,23 @@
  import {authConfig} from "./auth.config"
  import NextAuth from "next-auth"
  import { signIn } from "@/auth";
- export const { auth: middleware } = NextAuth(authConfig)
+//  export const { auth: middleware } = NextAuth(authConfig)
 import { login } from "./actions/login";
-// const { auth } = NextAuth(authConfig);
+export const { auth } = NextAuth(authConfig);
 const publicRoutes = [
     "/",
 ];
 const authRoutes = ['/dashboard/students']
 const apiAuthPrefix = "/api/auth";
- export default middleware(async (req) => {
+ export default auth(async (req) => {
  
     // const DEFAULT_LOGIN_REDIRECT = "/dashboard";
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
+    // console.log(isLoggedIn)
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
     if(isApiAuthRoute){
         return 
     }
@@ -31,6 +33,12 @@ const apiAuthPrefix = "/api/auth";
             nextUrl
         ));
     }
+    // if(isAuthRoute && req.auth?.role !== 'teacher' ){
+    //     return Response.redirect(new URL(
+    //         `/`,
+    //         nextUrl
+    //     )); 
+    // }
 
  })
  export const config = {
