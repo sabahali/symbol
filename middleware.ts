4e53c3ebf0e2b76fc080f0213bd30677854c3c75
mahalluapp <mahalluapp@gmail.com>
2024-02-27 14:51:44 +0530

    added middleware test

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,14 @@
  import {authConfig} from "./auth.config"
  import NextAuth from "next-auth"
- export const { auth: middleware } = NextAuth(authConfig)
+//  export const { auth: middleware } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
+
+ export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+    console.log(isLoggedIn)
+
+ })
+ export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  }
